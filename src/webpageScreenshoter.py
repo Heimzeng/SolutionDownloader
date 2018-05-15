d717a1903a3f8d64d4f80f6b81615bf3df7e81f3
heim <heimzeng@gmail.com>
2018-05-16 01:30:15 +0800

    v2.0

--- src/webpageScreenshoter.py
+++ src/webpageScreenshoter.py
@@ -40,6 +40,14 @@ def makeDirIfItsNotExist(dir_path):
 def checkFileExist(file_path):
     return os.path.isfile(file_path)
 
+def wait(driver):
+    while True:
+        documentState = driver.execute_script('return document.readyState')
+        jQueryState = driver.execute_script('return jQuery.active')
+        if documentState == 'complete' and jQueryState == 0:
+            sleep(1)
+            break
+
 if __name__ == '__main__':    
     login_url = 'http://www.chegg.com/login'
     with open('user.txt', 'r') as userFile:
@@ -62,28 +70,53 @@ if __name__ == '__main__':
     sleep(3)
     print('start!')
 
-    P_num =   [0, 53, 66, 81, 91, 85, 106, 69, 35, 49, 50, 45, 60, 53]
-    M_num =         [0, 10, 6,  6,  11, 6,  21,  12, 14, 33, 12, 11, 12, 4 ]
-    chapter_num = len(P_num)
-    for chapter_i_1 in range(chapter_num):
-        chapter_i = chapter_i_1 + 1
-        P_DIR = './solution/chapter' + str(chapter_i) + '/P/'
-        makeDirIfItsNotExist(P_DIR)
-        M_DIR = './solution/chapter' + str(chapter_i) + '/M/'
-        makeDirIfItsNotExist(M_DIR)
+    # test
+    b.driver.get('http://www.chegg.com/homework-help/Digital-Signal-Processing-4th-edition-chapter-2-problem-1P-solution-9780073380490')
+    wait(b.driver)
+    print('page getted')
+    #answerElement = b.driver.find_element_by_id('solution-player-sdk')
+    #ol = answerElement.find_element_by_tag_name('ol')
+    #chapters = ol.find_elements_by_tag_name('li')
+    #chapters_ol = b.driver.find_elements_by_class_name('chapters')
+    #chapters_lis = chapters_ol.find_elements_by_class_name('chapter')
+    jq_close_chapter_list = '$(".chapter.open h2").click()'
+    b.driver.execute_script(jq_close_chapter_list)
+    print("close open")
+    
+    jq_get_chapters_name = 'var chapterNameArray = new Array();                                                     \
+                            $(".chapters .chapter").each(function(index){                                           \
+                                                             chapterNameArray.push($(this).text());                 \
+                                                         });                                                        \
+                            return chapterNameArray;                                                                \
+                            '
+    chapters_name = b.driver.execute_script(jq_get_chapters_name)
+    print('chaptersname getted')
+
+    jq_get_problems_names = 'var chapters = new Array();                                                            \
+                            $(".chapters .chapter").each(function(index){                                           \
+                                                            $(this).find("h2").click();                             \
+                                                            var problemElementArray = new Array();                  \
+                                                            $(this).find(".problems .problem").each(function(index){\
+                                                                problemElementArray.push($(this).text());           \
+                                                            });                                                     \
+                                                            chapters.push(problemElementArray);                     \
+                                                         });                                                        \
+                            return chapters;                                                                        \
+                            '
+    problems_names = b.driver.execute_script(jq_get_problems_names)
+    print('problemsname getted')
+
+    for i, chapter_li_i, problems_name in zip(range(len(chapters_name)), chapters_name, problems_names):
+        chapter_i_name = chapters_name[i]
+        splits = chapter_i_name.split()
+        chapter_num = splits[1]
+        chapter_i_dir = './solution/chapter' + str(chapter_num) + '/'
+        makeDirIfItsNotExist(chapter_i_dir)
         elementId = 'solution-player-sdk'
-        for pro_i_1 in range(P_num[chapter_i_1]):
-            pro_i = pro_i_1 + 1
-            screenshotFilename = str(pro_i) + 'P' + '.png'
-            if not checkFileExist(P_DIR + screenshotFilename):
-                print('P:', chapter_i, pro_i)
-                url = 'http://www.chegg.com/homework-help/Digital-Signal-Processing-4th-edition-chapter-' + str(chapter_i) + '-problem-' + str(pro_i) + 'P-solution-9780073380490'
-                getElementScreenshot(b.driver, url, elementId, 'id', P_DIR, screenshotFilename)
-        for m_i_1 in range(M_num[chapter_i_1]):
-            m_i = m_i_1 + 1
-            screenshotFilename = str(m_i) + 'M' + '.png'
-            if not checkFileExist(M_DIR + screenshotFilename):
-                print('M:', chapter_i, m_i)
-                url = 'http://www.chegg.com/homework-help/Digital-Signal-Processing-4th-edition-chapter-' + str(chapter_i) + '-problem-' + str(m_i) + 'M-solution-9780073380490'
-                getElementScreenshot(b.driver, url, elementId, 'id', M_DIR, screenshotFilename)
+        for problem_name in problems_name:
+            screenshotFilename = problem_name + '.png'
+            if not checkFileExist(chapter_i_dir + screenshotFilename):
+                print(chapter_i_name, problem_name)
+                url = 'http://www.chegg.com/homework-help/Digital-Signal-Processing-4th-edition-chapter-' + str(chapter_num) + '-problem-' + problem_name + '-solution-9780073380490'
+                getElementScreenshot(b.driver, url, elementId, 'id', chapter_i_dir, screenshotFilename)
     b.quit()
